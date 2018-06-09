7c81b466646bc9c9f47ec8bc10e5cf52de6d4a67
leanfj <leanfj@hotmail.com>
2018-06-08 22:04:20 -0300

    update

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,9 @@
+class Negociacao {
+  //Definir propriedades de classes
+  //parametros podem ser definidos na funçao constructor
+  constructor(data, quantidade, valor){
+    this.data = data;
+    this.quantidade = quantidade;
+    this.valor = valor;
+  }
+}
