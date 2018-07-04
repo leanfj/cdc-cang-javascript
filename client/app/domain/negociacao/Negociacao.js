95d13fcf4ab5ea0f00daf776cb2aea7ef17ee1b6
leanfj <leanfj@hotmail.com>
2018-07-03 23:36:08 -0300

    Encapsulamento

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,15 +1,27 @@
 class Negociacao {
   //Definir propriedades de classes
   //parametros podem ser definidos na funçao constructor
-  constructor(data, quantidade, valor){
-    this.data = data;
-    this.quantidade = quantidade;
-    this.valor = valor;
+  constructor(data, quantidade, valor) {
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
   }
 
   //Metodos
   //Functions definindo comportamento de uma classe
-  obterVolume() {
-    return this.quantidade * this.valor;
+  getVolume() {
+    return this._quantidade * this._valor;
+  }
+
+  //Metodos de Acesso a propriedades prefixadas com _ 
+
+  getData() {
+    return this._data;
+  }
+  getQuantidade() {
+    return this._quantidade;
+  }
+  getValor() {
+    return this._valor;
   }
 }
