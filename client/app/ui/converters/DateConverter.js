656ccd18bd6c8ff6f978fa6578585b67345b4755
leanfj <leanfj@hotmail.com>
2018-07-11 00:35:40 -0300

    Criando classes estaticas

---
+++ client/app/ui/converters/DateConverter.js
@@ -0,0 +1,14 @@
+class DateConverter {
+
+	constructor(){
+		throw new Error ('Classe não pode ser instanciada');
+	}
+	//Podemos chamar metodos diretamente de classe quando declarados com o static para metodos que não manipulam propriedade de instacias
+	static toText (data) {
+		return data.getDate() + '/' + data.getMonth() + '/' + data.getFullYear();
+	}
+
+	static toDate (text) {
+		return new Date(...text.split('-').map((item, index) => item - index % 2));
+	}
+}
