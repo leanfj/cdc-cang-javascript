3a70a4ddfa52b57fdc4374c4295f3070716c69a2
leanfj <leanfj@hotmail.com>
2018-07-24 00:32:41 -0300

    Modelo negociacoes para lista

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,47 +1,45 @@
 class NegociacaoController {
-	constructor(){
-		//Busca adcionadas ao constructor para evitar pecorrer o DOM
-		//bind para manter o contexto do this em document
-		let $ = document.querySelector.bind(document);
+  constructor() {
+    //Busca adcionadas ao constructor para evitar pecorrer o DOM
+    //bind para manter o contexto do this em document
+    let $ = document.querySelector.bind(document);
 
-		//elementos HTML
-		this._inputData = $('#data');
-		this._inputQuantidade = $('#quantidade');
-		this._inputValor = $('#valor');
+    //elementos HTML
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
+    this._negociacoes = new Negociacoes();
+  }
+  adciona(event) {
+    //Previnir evento padrão
+    event.preventDefault();
 
-	}
-	adciona (event) {
-		//Previnir evento padrão
-		event.preventDefault();
+    // let date = this._inputData.value;
+    // //Utilizar metodo split para criar um arrays separando a string no indicador
+    // let formatedDate = date.split('-');
+    // console.log(formatedDate);
+    // //Juntando elementos do array em uma string com elemento separados pelo indicador
+    // formatedDate = formatedDate.join(',');
+    // console.log(formatedDate);
+    // //new Date pode receber como parametros um arrays ou string separados por virgula
+    // let data = new Date(formatedDate);
+    // console.log(data);
 
-		// let date = this._inputData.value;
-		// //Utilizar metodo split para criar um arrays separando a string no indicador
-		// let formatedDate = date.split('-');
-		// console.log(formatedDate);
-		// //Juntando elementos do array em uma string com elemento separados pelo indicador
-		// formatedDate = formatedDate.join(',');
-		// console.log(formatedDate);
-		// //new Date pode receber como parametros um arrays ou string separados por virgula
-		// let data = new Date(formatedDate);
-		// console.log(data);
+    //spread operator separa o array para ser trabalhado com cada um dos seus elementos
+    //Utilização de nova calsse de converção criada
+    // let converter = new DateConverter();
 
-		//spread operator separa o array para ser trabalhado com cada um dos seus elementos
-		//Utilização de nova calsse de converção criada
-		// let converter = new DateConverter();
+    let date = DateConverter.toDate(this._inputData.value);
+    //Arrow functions nos permiti omitir {} e o return quando se trata de apena uma instrução
 
+    let negociacao = new Negociacao(
+      date,
+      parseInt(this._inputQuantidade.value),
+      parseFloat(this._inputValor.value)
+    );
 
-		let date = DateConverter.toDate(this._inputData.value);
-		//Arrow functions nos permiti omitir {} e o return quando se trata de apena uma instrução
+    this._negociacoes.adiciona(negociacao);
 
-		let negociacao = new Negociacao(
-			date,
-			parseInt(this._inputQuantidade.value),
-			parseFloat(this._inputValor.value)
-		);
-
-		let diaMesAno = DateConverter.toText(negociacao.data);
-
-		console.log(diaMesAno);
-
-	}
+    console.log(this._negociacoes.paraArray());
+  }
 }
--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -9,6 +9,9 @@ class DateConverter {
 	}
 
 	static toDate (text) {
+		if(!/^\d{4}-\d{2}-\d{2}$/.test(text)){
+			throw new Error('Deve estar no formato aaaa-mm-dd');
+		}
 		return new Date(...text.split('-').map((item, index) => item - index % 2));
 	}
 }
