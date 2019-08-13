d56bef03b758d8f458fc39294a79b79f3ac5a266
leanfj <leanfj@hotmail.com>
2019-08-13 00:16:43 -0300

    Usando JSON.stringify para comparar objetos

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -30,12 +30,14 @@ class Negociacao {
   }
 
   equals(negociacao) {
-    return (
-      this.data.getDate() == negociacao.data.getDate() &&
-      this.data.getMonth() == negociacao.data.getMonth() &&
-      this.data.getFullYear() == negociacao.data.getFullYear() &&
-      this.quantidade == negociacao.quantidade &&
-      this.valor == negociacao.valor
-    );
+    // return (
+    //   this.data.getDate() == negociacao.data.getDate() &&
+    //   this.data.getMonth() == negociacao.data.getMonth() &&
+    //   this.data.getFullYear() == negociacao.data.getFullYear() &&
+    //   this.quantidade == negociacao.quantidade &&
+    //   this.valor == negociacao.valor
+    // );
+
+    return JSON.stringify(this) == JSON.stringify(negociacao);
   }
 }
