59441cd419a465a5d76d6e6ac27f53d1d45ba5c7
leanfj <leanfj@hotmail.com>
2019-08-13 00:08:36 -0300

    Comparação de negociacões por valor

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -4,10 +4,11 @@ class Negociacao {
   constructor(_data, _quantidade, _valor) {
     //Copia de propriedades
     //Variaveis de argumentos passada para copia em objeto literais Es06
-    Object.assign(this, {_quantidade, _valor});
+    Object.assign(this, { _quantidade, _valor });
     this._data = new Date(_data.getTime());
     //Congelando objeto
-    Object.freeze(this);  }
+    Object.freeze(this);
+  }
 
   //Metodos
   //Functions definindo comportamento de uma classe
@@ -15,11 +16,10 @@ class Negociacao {
     return this._quantidade * this._valor;
   }
 
-  //Metodos de Acesso a propriedades prefixadas com _ 
+  //Metodos de Acesso a propriedades prefixadas com _
 
   //Sintaze get para criar propriedade getters para aceso de somente leitura
   get data() {
-
     return new Date(this._data.getTime());
   }
   get quantidade() {
@@ -27,5 +27,15 @@ class Negociacao {
   }
   get valor() {
     return this._valor;
+  }
+
+  equals(negociacao) {
+    return (
+      this.data.getDate() == negociacao.data.getDate() &&
+      this.data.getMonth() == negociacao.data.getMonth() &&
+      this.data.getFullYear() == negociacao.data.getFullYear() &&
+      this.quantidade == negociacao.quantidade &&
+      this.valor == negociacao.valor
+    );
   }
 }
