52d2b7c845354b2e79a1bfe443d4445b8f7aa231
leanfj <leanfj@hotmail.com>
2019-08-25 22:41:58 -0300

    Criado metodo para manipular stores, passagem de result para sucesso e error

--- client/app/util/ConnectionFactory.js
+++ client/app/util/ConnectionFactory.js
@@ -1,8 +1,35 @@
+const stores = ["negociacoes"];
+
 class ConnectionFactory {
   constructor() {
     throw new Error("Não é possível criar instâncias dessa Classe");
   }
   static getConnection() {
-    return new Promise((resolve, reject) => {});
+    return new Promise((resolve, reject) => {
+      const openRequest = indexedDB.open("jscangaceiro", 2);
+
+      openRequest.onupgradeneeded = e => {
+        ConnectionFactory._createStores(e.target.result);
+      };
+
+      openRequest.onsuccess = e => {
+        resolve(e.target.result);
+      };
+
+      openRequest.onerror = e => {
+        console.log(e.target.error);
+        reject(e.target.error.name);
+      };
+    });
+  }
+
+  static _createStores(connection) {
+    stores.forEach(store => {
+      if (connection.objectStoreNames.contains(store)) {
+        connection.deleteObjectStore(store);
+      }
+
+      connection.createObjectStore(store, { autoIncrement: true });
+    });
   }
 }
