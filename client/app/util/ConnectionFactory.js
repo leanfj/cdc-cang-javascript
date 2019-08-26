4f398c6529bb9f6191bb8ad1b4450d2728fe05d6
leanfj <leanfj@hotmail.com>
2019-08-25 22:57:39 -0300

    Utilização de module pattern com IIFE

--- client/app/util/ConnectionFactory.js
+++ client/app/util/ConnectionFactory.js
@@ -1,41 +1,43 @@
-const stores = ["negociacoes"];
-let connection = null;
-class ConnectionFactory {
-  constructor() {
-    throw new Error("Não é possível criar instâncias dessa Classe");
-  }
-  static getConnection() {
-    return new Promise((resolve, reject) => {
-      if (connection) {
-        return resolve(connection);
-      }
+const ConnectionFactory = (() => {
+  const stores = ["negociacoes"];
+  let connection = null;
+  return class ConnectionFactory {
+    constructor() {
+      throw new Error("Não é possível criar instâncias dessa Classe");
+    }
+    static getConnection() {
+      return new Promise((resolve, reject) => {
+        if (connection) {
+          return resolve(connection);
+        }
 
-      const openRequest = indexedDB.open("jscangaceiro", 2);
+        const openRequest = indexedDB.open("jscangaceiro", 2);
 
-      openRequest.onupgradeneeded = e => {
-        ConnectionFactory._createStores(e.target.result);
-      };
+        openRequest.onupgradeneeded = e => {
+          ConnectionFactory._createStores(e.target.result);
+        };
 
-      openRequest.onsuccess = e => {
-        connection = e.target.result;
+        openRequest.onsuccess = e => {
+          connection = e.target.result;
 
-        resolve(e.target.result);
-      };
+          resolve(e.target.result);
+        };
 
-      openRequest.onerror = e => {
-        console.log(e.target.error);
-        reject(e.target.error.name);
-      };
-    });
-  }
+        openRequest.onerror = e => {
+          console.log(e.target.error);
+          reject(e.target.error.name);
+        };
+      });
+    }
 
-  static _createStores(connection) {
-    stores.forEach(store => {
-      if (connection.objectStoreNames.contains(store)) {
-        connection.deleteObjectStore(store);
-      }
+    static _createStores(connection) {
+      stores.forEach(store => {
+        if (connection.objectStoreNames.contains(store)) {
+          connection.deleteObjectStore(store);
+        }
 
-      connection.createObjectStore(store, { autoIncrement: true });
-    });
-  }
-}
+        connection.createObjectStore(store, { autoIncrement: true });
+      });
+    }
+  };
+})();
