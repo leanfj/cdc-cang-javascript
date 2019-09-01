fab7863531acebc39a6288041c6aabc3417a6c84
leanfj <leanfj@hotmail.com>
2019-09-01 11:41:18 -0300

    Teste com api fetch fazendo POST

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,5 +1,26 @@
 import { NegociacaoController } from "./controllers/NegociacaoController.js";
 
+import { Negociacao } from "./domain/index.js";
+
+const negociacao = new Negociacao(new Date(), 1, 100);
+
+const headers = new Headers();
+headers.set("Content-Type", "application/json");
+
+const body = JSON.stringify(negociacao);
+
+const method = "POST";
+
+const config = {
+  method,
+  headers,
+  body
+};
+
+fetch("/negociacoes", config).then(() => {
+  console.log("Dado enviado com sucesso");
+});
+
 //Instacia do Controller
 const controller = new NegociacaoController();
 
